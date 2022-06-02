f15ffc9b80c73a61a28b68f8a48028813c163bc3
Leonid Prokopev <leo.messi32@yandex.ru>
2022-06-02 15:38:13 +0300

    fix bugs,need to enable posts

--- project_practice_10/blog/models.py
+++ project_practice_10/blog/models.py
@@ -17,16 +17,12 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='blog_posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2,
-                              choices=Status.choices,
-                              default=Status.DRAFT)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
 
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
