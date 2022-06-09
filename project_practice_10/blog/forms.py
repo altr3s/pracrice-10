56e0501ac0cba60367e90e2a4185ec0a1c9daf63
Leonid Prokopev <leo.messi32@yandex.ru>
2022-06-09 16:45:51 +0300

    fix codestyle

--- project_practice_10/blog/forms.py
+++ project_practice_10/blog/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import Comment
+
+
 class CommentForm(forms.ModelForm):
-	class Meta:
-		model = Comment
-		fields = ['name', 'email', 'body']
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
